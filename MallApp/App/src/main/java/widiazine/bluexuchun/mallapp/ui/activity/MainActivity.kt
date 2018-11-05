06460619a4968f688e92198c959563a57041a9e7
bluexuchun <345065695@qq.com>
2018-11-05 14:36:11 +0800

    project mall

---
+++ MallApp/App/src/main/java/widiazine/bluexuchun/mallapp/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package widiazine.bluexuchun.mallapp.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import widiazine.bluexuchun.mallapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- SendSms/app/src/main/java/com/widiazine/bluexuchun/sendsms/MainActivity.kt
+++ SendSms/app/src/main/java/com/widiazine/bluexuchun/sendsms/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.widiazine.bluexuchun.sendsms
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.telephony.SmsManager
 import android.view.View
 import android.widget.Button
@@ -11,7 +14,7 @@ import android.widget.Toast
 /**
  * 发送短信
  */
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
 
     var phone:EditText? = null
     var content:EditText? = null
@@ -29,8 +32,11 @@ class MainActivity : BaseActivity() {
         var send = findViewById<Button>(R.id.bt_click)
 
         send.setOnClickListener {
-//            sendSms()
-            requestStoragePermissions()
+            if(hasWriteExternalStoragePermission()){
+                sendSms()
+            }else{
+                applyWriteExternalStoragePermission()
+            }
         }
 
     }
@@ -48,8 +54,30 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    override fun allowSuccess() {
-        super.allowSuccess()
-        sendSms()
+    /**
+     * 申请权限
+     */
+    public fun applyWriteExternalStoragePermission(){
+        val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(this@MainActivity,permissions,0)
+    }
+
+    /**
+     * 检查是否有写磁盘的权限
+     */
+    public fun hasWriteExternalStoragePermission():Boolean{
+        val result = ActivityCompat.checkSelfPermission(this@MainActivity, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            // 用户同意权限
+            Toast.makeText(this@MainActivity,"Permission Allow",Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this@MainActivity,"Permission Denied",Toast.LENGTH_SHORT).show()
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
     }
 }
